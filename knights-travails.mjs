8225c2b129594546d0e6d7640758c404f647aac6
Juneid Teklu <te.juneid@gmail.com>
2024-09-20 20:11:25 -0500

    Make each chess square into a vertex instance

--- knights-travails.mjs
+++ knights-travails.mjs
@@ -1,6 +1,8 @@
 class Vertex {
   constructor(data) {
     this.data = data;
+    this.previous = null;
+    this.steps = null;
     this.adjacentIndices = [];
   }
   addAdjacentIndices(vertex) {
@@ -129,84 +131,84 @@ function shortestPath(startingCity, finalDestination) {
 
 let chessboard = [
   [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [0, 4],
-    [0, 5],
-    [0, 6],
-    [0, 7],
+    new Vertex([0, 0]),
+    new Vertex([0, 1]),
+    new Vertex([0, 2]),
+    new Vertex([0, 3]),
+    new Vertex([0, 4]),
+    new Vertex([0, 5]),
+    new Vertex([0, 6]),
+    new Vertex([0, 7]),
   ],
   [
-    [1, 0],
-    [1, 1],
-    [1, 2],
-    [1, 3],
-    [1, 4],
-    [1, 5],
-    [1, 6],
-    [1, 7],
+    new Vertex([1, 0]),
+    new Vertex([1, 1]),
+    new Vertex([1, 2]),
+    new Vertex([1, 3]),
+    new Vertex([1, 4]),
+    new Vertex([1, 5]),
+    new Vertex([1, 6]),
+    new Vertex([1, 7]),
   ],
   [
-    [2, 0],
-    [2, 1],
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [2, 5],
-    [2, 6],
-    [2, 7],
+    new Vertex([2, 0]),
+    new Vertex([2, 1]),
+    new Vertex([2, 2]),
+    new Vertex([2, 3]),
+    new Vertex([2, 4]),
+    new Vertex([2, 5]),
+    new Vertex([2, 6]),
+    new Vertex([2, 7]),
   ],
   [
-    [3, 0],
-    [3, 1],
-    [3, 2],
-    [3, 3],
-    [3, 4],
-    [3, 5],
-    [3, 6],
-    [3, 7],
+    new Vertex([3, 0]),
+    new Vertex([3, 1]),
+    new Vertex([3, 2]),
+    new Vertex([3, 3]),
+    new Vertex([3, 4]),
+    new Vertex([3, 5]),
+    new Vertex([3, 6]),
+    new Vertex([3, 7]),
   ],
   [
-    [4, 0],
-    [4, 1],
-    [4, 2],
-    [4, 3],
-    [4, 4],
-    [4, 5],
-    [4, 6],
-    [4, 7],
+    new Vertex([4, 0]),
+    new Vertex([4, 1]),
+    new Vertex([4, 2]),
+    new Vertex([4, 3]),
+    new Vertex([4, 4]),
+    new Vertex([4, 5]),
+    new Vertex([4, 6]),
+    new Vertex([4, 7]),
   ],
   [
-    [5, 0],
-    [5, 1],
-    [5, 2],
-    [5, 3],
-    [5, 4],
-    [5, 5],
-    [5, 6],
-    [5, 7],
+    new Vertex([5, 0]),
+    new Vertex([5, 1]),
+    new Vertex([5, 2]),
+    new Vertex([5, 3]),
+    new Vertex([5, 4]),
+    new Vertex([5, 5]),
+    new Vertex([5, 6]),
+    new Vertex([5, 7]),
   ],
   [
-    [6, 0],
-    [6, 1],
-    [6, 2],
-    [6, 3],
-    [6, 4],
-    [6, 5],
-    [6, 6],
-    [6, 7],
+    new Vertex([6, 0]),
+    new Vertex([6, 1]),
+    new Vertex([6, 2]),
+    new Vertex([6, 3]),
+    new Vertex([6, 4]),
+    new Vertex([6, 5]),
+    new Vertex([6, 6]),
+    new Vertex([6, 7]),
   ],
   [
-    [7, 0],
-    [7, 1],
-    [7, 2],
-    [7, 3],
-    [7, 4],
-    [7, 5],
-    [7, 6],
-    [7, 7],
+    new Vertex([7, 0]),
+    new Vertex([7, 1]),
+    new Vertex([7, 2]),
+    new Vertex([7, 3]),
+    new Vertex([7, 4]),
+    new Vertex([7, 5]),
+    new Vertex([7, 6]),
+    new Vertex([7, 7]),
   ],
 ];
 
@@ -226,6 +228,7 @@ function knightsTravails(start, end) {
   let current;
   let queue = [];
   let visited = [];
+  chessboard[start[0]][start[1]].steps = 0;
 
   /* Loop that goes through all vertices to determine which follow the rules of knights movement
   starting from start and add all valid vertices to starts adjacentVertices array */
@@ -241,26 +244,29 @@ function knightsTravails(start, end) {
     if (current === end) {
       return "You reached the end";
     }
-    let col = current[0];
-    let row = current[1];
-    let adjacent;
+    let col = current.data[0];
+    let row = current.data[1];
 
     for (let i = 0; i < 8; i++) {
       if (i == 0) {
         if (
-          current[0] + 2 > 7 ||
-          current[0] + 2 < 0 ||
-          current[1] + 1 > 7 ||
-          current[1] + 1 < 0
+          current.data[0] + 2 > 7 ||
+          current.data[0] + 2 < 0 ||
+          current.data[1] + 1 > 7 ||
+          current.data[1] + 1 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col + 2][row + 1]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col + 2][row + 1].data) ===
+          JSON.stringify(end)
         ) {
           return "You reached the end";
         } else if (visited.includes(chessboard[col + 2][row + 1])) {
           continue;
         } else {
+          chessboard[start[0]][start[1]].previous = current;
+          chessboard[start[0]][start[1]].steps = current.steps + 1;
+
           queue.push(chessboard[col + 2][row + 1]);
 
           continue;
@@ -268,133 +274,157 @@ function knightsTravails(start, end) {
       }
       if (i == 1) {
         if (
-          current[0] + 2 > 7 ||
-          current[0] + 2 < 0 ||
-          current[1] - 1 > 7 ||
-          current[1] - 1 < 0
+          current.data[0] + 2 > 7 ||
+          current.data[0] + 2 < 0 ||
+          current.data[1] - 1 > 7 ||
+          current.data[1] - 1 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col + 2][row - 1]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col + 2][row - 1].data) ===
+          JSON.stringify(end)
         ) {
+          /* Instead of returning a string, make end reference the previous step and add a 1 to total step count and then return a string
+          that says ends step count*/
           return "You found the end";
         } else if (visited.includes(chessboard[col + 2][row - 1])) {
           continue;
         } else {
+          chessboard[col + 2][row - 1].previous = current;
+          chessboard[col + 2][row - 1].steps = current.steps + 1;
+
           queue.push(chessboard[col + 2][row - 1]);
           continue;
         }
       }
       if (i == 2) {
         if (
-          current[0] + 1 > 7 ||
-          current[0] + 1 < 0 ||
-          current[1] + 2 > 7 ||
-          current[1] + 2 < 0
+          current.data[0] + 1 > 7 ||
+          current.data[0] + 1 < 0 ||
+          current.data[1] + 2 > 7 ||
+          current.data[1] + 2 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col + 1][row + 2]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col + 1][row + 2].data) ===
+          JSON.stringify(end)
         ) {
           return "You found the end";
         } else if (visited.includes(chessboard[col + 1][row + 2])) {
           continue;
         } else {
+          chessboard[col + 1][row + 2].previous = current;
+          chessboard[col + 1][row + 2].steps = current.steps + 1;
           queue.push(chessboard[col + 1][row + 2]);
           continue;
         }
       }
       if (i == 3) {
         if (
-          current[0] + 1 > 7 ||
-          current[0] + 1 < 0 ||
-          current[1] - 2 > 7 ||
-          current[1] - 2 < 0
+          current.data[0] + 1 > 7 ||
+          current.data[0] + 1 < 0 ||
+          current.data[1] - 2 > 7 ||
+          current.data[1] - 2 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col + 1][row - 2]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col + 1][row - 2].data) ===
+          JSON.stringify(end)
         ) {
           return "You found the end";
         } else if (visited.includes(chessboard[col + 1][row - 2])) {
           continue;
         } else {
+          chessboard[col + 1][row - 2].previous = current;
+          chessboard[col + 1][row - 2].steps = current.steps + 1;
           queue.push(chessboard[col + 1][row - 2]);
           continue;
         }
       }
       if (i == 4) {
         if (
-          current[0] - 2 > 7 ||
-          current[0] - 2 < 0 ||
-          current[1] + 1 > 7 ||
-          current[1] + 1 < 0
+          current.data[0] - 2 > 7 ||
+          current.data[0] - 2 < 0 ||
+          current.data[1] + 1 > 7 ||
+          current.data[1] + 1 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col - 2][row + 1]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col - 2][row + 1].data) ===
+          JSON.stringify(end)
         ) {
           return "You found the end";
         } else if (visited.includes(chessboard[col - 2][row + 1])) {
           continue;
         } else {
+          chessboard[col - 2][row + 1].previous = current;
+          chessboard[col - 2][row + 1].steps = current.steps + 1;
           queue.push(chessboard[col - 2][row + 1]);
           continue;
         }
       }
       if (i == 5) {
         if (
-          current[0] - 2 > 7 ||
-          current[0] - 2 < 0 ||
-          current[1] - 1 > 7 ||
-          current[1] - 1 < 0
+          current.data[0] - 2 > 7 ||
+          current.data[0] - 2 < 0 ||
+          current.data[1] - 1 > 7 ||
+          current.data[1] - 1 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col - 2][row - 1]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col - 2][row - 1].data) ===
+          JSON.stringify(end)
         ) {
           return "You found the end";
         } else if (visited.includes(chessboard[col - 2][row - 1])) {
           continue;
         } else {
+          chessboard[col - 2][row - 1].previous = current;
+          chessboard[col - 2][row - 1].steps = current.steps + 1;
           queue.push(chessboard[col - 2][row - 1]);
           continue;
         }
       }
       if (i == 6) {
         if (
-          current[0] - 1 > 7 ||
-          current[0] - 1 < 0 ||
-          current[1] + 2 > 7 ||
-          current[1] + 2 < 0
+          current.data[0] - 1 > 7 ||
+          current.data[0] - 1 < 0 ||
+          current.data[1] + 2 > 7 ||
+          current.data[1] + 2 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col - 1][row + 2]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col - 1][row + 2].data) ===
+          JSON.stringify(end)
         ) {
           return "You found the end";
         } else if (visited.includes(chessboard[col - 1][row + 2])) {
           continue;
         } else {
+          chessboard[col - 1][row + 2].previous = current;
+          chessboard[col - 1][row + 2].steps = current.steps + 1;
           queue.push(chessboard[col - 1][row + 2]);
           continue;
         }
       }
       if (i == 7) {
         if (
-          current[0] - 1 > 7 ||
-          current[0] - 1 < 0 ||
-          current[1] - 2 > 7 ||
-          current[1] - 2 < 0
+          current.data[0] - 1 > 7 ||
+          current.data[0] - 1 < 0 ||
+          current.data[1] - 2 > 7 ||
+          current.data[1] - 2 < 0
         ) {
           continue;
         } else if (
-          JSON.stringify(chessboard[col - 1][row - 2]) === JSON.stringify(end)
+          JSON.stringify(chessboard[col - 1][row - 2].data) ===
+          JSON.stringify(end)
         ) {
           return "You found the end";
         } else if (visited.includes(chessboard[col - 1][row - 2])) {
           continue;
         } else {
+          chessboard[col - 1][row - 2].previous = current;
+          chessboard[col - 1][row - 2].steps = current.steps;
           queue.push(chessboard[col - 1][row - 2]);
           continue;
         }
